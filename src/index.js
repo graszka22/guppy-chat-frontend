a007b2403875a80049345bc3b136c9433f1e107b
Angelika Serwa <graszka223@gmail.com>
2018-12-31 00:10:57 +0100

    Add redux-persist

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware, compose } from "redux";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react'
 import createSagaMiddleware from "redux-saga";
 import { Provider } from "react-redux";
 import mainSaga from "./saga";
@@ -12,14 +15,29 @@ import reducer from "./reducers";
 const sagaMiddleware = createSagaMiddleware();
 const reduxDevTools =
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducer)
+
 let store = createStore(
-    reducer,
+  persistedReducer,
     compose(applyMiddleware(sagaMiddleware), reduxDevTools)
 );
 
+let persistor = persistStore(store)
+
 sagaMiddleware.run(mainSaga);
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
